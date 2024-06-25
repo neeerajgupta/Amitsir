fd361e369bbc4937ec52133c32671a5baec1abbf
Neeraj Gupta <soft.guptaneeraj@gmail.com>
2024-06-25 22:14:58 +0530

    dashbord

--- src/app/components/dashbord/dashbord.component.ts
+++ src/app/components/dashbord/dashbord.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CarouselModule } from 'primeng/carousel';
+import { ImageModule } from 'primeng/image';
+import { AutoCompleteCompleteEvent, AutoCompleteModule } from 'primeng/autocomplete';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from "../footer/footer.component";
 
 @Component({
@@ -11,13 +14,56 @@ import { FooterComponent } from "../footer/footer.component";
     imports: [
         CarouselModule,
         ButtonModule,
+        ImageModule,
+        AutoCompleteModule,
+        FormsModule,
+        ReactiveFormsModule,
         FooterComponent
     ]
 })
 export class DashbordComponent {
- 
-  }
+    filteredData: string[] = [];
+    dummyData: string[] = [];
+    selectedItem: string | undefined;
+  
+    constructor() {}
+  
+    ngOnInit() {
+      this.dummyData = [
+        'Apple',
+        'Banana',
+        'Cherry',
+        'Date',
+        'Fig',
+        'Grape',
+        'Kiwi',
+        'Lemon',
+        'Mango',
+        'Orange',
+        'Papaya',
+        'Quince',
+        'Raspberry',
+        'Strawberry',
+        'Tomato',
+        'Ugli fruit',
+        'Watermelon'
+      ];
+    }
+  
 
- 
+    filterData(event: AutoCompleteCompleteEvent) {
+        let filtered: any[] = [];
+        let query = event.query;
+
+        for (let i = 0; i < (this.dummyData as any[]).length; i++) {
+            let country = (this.dummyData as any[])[i];
+            if (country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+                filtered.push(country);
+            }
+        }
+
+        this.filteredData = filtered;
+    }
+  }
 
 
